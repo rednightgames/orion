e1c437c7b5509aa96171e30a5b132bd964b1f17c
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2024-04-28 17:53:36 +0200

    fix: fixed camera rotation

--- src/Player.cs
+++ src/Player.cs
@@ -3,16 +3,20 @@ using Godot;
 public partial class Player : CharacterBody3D
 {
     public float speed = 160.0f;
-    private AnimationTree Anim;
-    private AnimationNodeStateMachinePlayback StateMachine;
-    private PlayerCamera Camera;
+    private const int cameraRotationAngle = 45;
+    private const float cameraRotationDuration = 0.3f;
+    private AnimationTree _anim;
+    private AnimationNodeStateMachinePlayback _stateMachine;
+    private PlayerCamera _playerCamera;
     private Vector3 _rotation;
+    private Tween _cameraTween;
+    private bool _isCameraRotating = false;
 
     public override void _Ready()
     {
-        Camera = GetNode<PlayerCamera>("PlayerCamera");
-        Anim = GetNode<AnimationTree>("AnimationTree");
-        StateMachine = (AnimationNodeStateMachinePlayback)Anim.Get("parameters/playback");
+        _anim = GetNode<AnimationTree>("AnimationTree");
+        _stateMachine = (AnimationNodeStateMachinePlayback)_anim.Get("parameters/playback");
+        _playerCamera = GetNode<PlayerCamera>("PlayerCamera");
     }
 
     public override void _Input(InputEvent @event)
@@ -22,15 +26,11 @@ public partial class Player : CharacterBody3D
             switch (mouseEvent.ButtonIndex)
             {
                 case MouseButton.WheelUp:
-                {
-                    Camera.SetCameraZoom(Camera.Zoom * 1.2f);
+                    _playerCamera.SetCameraZoom(_playerCamera.Zoom * 1.2f);
                     break;
-                }
                 case MouseButton.WheelDown:
-                {
-                    Camera.SetCameraZoom(Camera.Zoom * 0.8f);
+                    _playerCamera.SetCameraZoom(_playerCamera.Zoom * 0.8f);
                     break;
-                }
             }
         }
     }
@@ -43,40 +43,59 @@ public partial class Player : CharacterBody3D
             Input.GetActionStrength("move_backward") - Input.GetActionStrength("move_forward")
         );
 
-        Vector3 rotatedInputDirection = inputDirection.Rotated(Vector3.Up, Rotation.Y).Normalized();
-
-        inputDirection = inputDirection.Normalized();
-
-        Velocity = rotatedInputDirection * speed * (float)delta;
-
-        if (Input.IsActionJustPressed("Q"))
+        if (inputDirection != Vector3.Zero)
         {
-            _rotation = new Vector3(0, Rotation.Y - 45 * Mathf.Pi / 180, 0);
-        }
-        if (Input.IsActionJustPressed("E"))
-        {
-            _rotation = new Vector3(0, Rotation.Y + 45 * Mathf.Pi / 180, 0);
-        }
-        Tween tween_rotation = GetTree().CreateTween();
-        tween_rotation.TweenProperty(this, "rotation", _rotation, 0.3);
+            Vector3 rotatedInputDirection = inputDirection
+                .Rotated(Vector3.Up, Rotation.Y)
+                .Normalized();
+            inputDirection = inputDirection.Normalized();
+            Velocity = rotatedInputDirection * speed * (float)delta;
 
-        if (inputDirection == Vector3.Zero)
-        {
-            StateMachine.Travel("idle");
-        }
-        else
-        {
-            StateMachine.Travel("walk");
-            Anim.Set(
+            _stateMachine.Travel("walk");
+            _anim.Set(
                 "parameters/idle/blend_position",
                 new Vector3(inputDirection.X, inputDirection.Z, inputDirection.Y)
             );
-            Anim.Set(
+            _anim.Set(
                 "parameters/walk/blend_position",
                 new Vector3(inputDirection.X, inputDirection.Z, inputDirection.Y)
             );
 
             MoveAndSlide();
         }
+        else
+        {
+            _stateMachine.Travel("idle");
+        }
+
+        if (Input.IsActionPressed("Q") && !_isCameraRotating)
+        {
+            RotateCamera(-cameraRotationAngle);
+        }
+        else if (Input.IsActionPressed("E") && !_isCameraRotating)
+        {
+            RotateCamera(cameraRotationAngle);
+        }
+    }
+
+    private void RotateCamera(float angle)
+    {
+        _rotation.Y += Mathf.DegToRad(angle);
+        _cameraTween = CreateTween().SetParallel(true);
+        _cameraTween.TweenProperty(
+            this,
+            "rotation",
+            new Vector3(Rotation.X, _rotation.Y, Rotation.Z),
+            cameraRotationDuration
+        );
+        _isCameraRotating = true;
+        _cameraTween.Connect("finished", new Callable(this, nameof(OnCameraRotationCompleted)));
+    }
+
+    private void OnCameraRotationCompleted()
+    {
+        _isCameraRotating = false;
+        _rotation.Y = Mathf.Wrap(Rotation.Y, -Mathf.Pi, Mathf.Pi);
+        Rotation = new Vector3(Rotation.X, _rotation.Y, Rotation.Z);
     }
 }
