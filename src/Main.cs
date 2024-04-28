ebca3ae03ab0e9d6bdb1fdc9215dc70a89494c38
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2024-04-28 20:54:19 +0200

    feat: update godot to 4.2.2

--- src/Main.cs
+++ src/Main.cs
@@ -4,13 +4,17 @@ public partial class Main : Node3D
 {
     [Export]
     public Vector3 SpawnPosition = new Vector3(0, 0, 0);
-
+   
+    
     public override void _Ready()
     {
         Create_player();
+        
     }
 
-    public override void _Process(double delta) { }
+    public override void _Process(double delta) 
+    {  
+    }
 
     public void Create_player()
     {
@@ -18,4 +22,5 @@ public partial class Main : Node3D
         player.Position = SpawnPosition;
         AddChild(player);
     }
+    
 }
--- src/UI.cs
+++ src/UI.cs
@@ -2,17 +2,23 @@ using Godot;
 
 public partial class UI : Control
 {
-    private ProgressBar _hungerScale;
-    private Entity _entity;
 
-    public override void _Ready()
-    {
-        _hungerScale = GetNode<ProgressBar>("HungerScale");
-        _entity = GetNode<Entity>("../Entity");
-    }
-
-    public override void _Process(double delta)
-    {
-        _hungerScale.Value = _entity._hungryValue;
-    }
+	private ProgressBar _hungerScale;
+	private Entity _entity;
+	private Label _timeDayUI;
+	private DayNightCycle _dayNightCycle;
+	
+	public override void _Ready()
+	{
+		_hungerScale = GetNode<ProgressBar>("HungerScale");
+		_entity = GetNode<Entity>("../Entity");
+		_timeDayUI = GetNode<Label>("TimeOfDay");
+		_dayNightCycle = GetNode<DayNightCycle>("../DayNightCycle");
+	}
+	public override void _Process(double delta)
+	{
+		GD.Print(_timeDayUI);
+		_hungerScale.Value = _entity._hungryValue;
+		_timeDayUI.Text = _dayNightCycle._timeDay;
+	}
 }
